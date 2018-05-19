ba0f8ea95d8e0aceae2360a515a3b53a50f856ca
lol <lol@lol-X550LA>
2018-05-19 20:31:09 +0200

    Añadida bd.modelo.dao.Alquileres

---
+++ Uroz_LealdeIbarra_Diego_PROG_Tarea10/src/alquilervehiculos/mvc/modelo/bd/dao/Alquileres.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alquilervehiculos.mvc.modelo.bd.dao;
+
+import alquilervehiculos.mvc.modelo.ficheros.dao.*;
+import alquilervehiculos.mvc.modelo.dominio.Alquiler;
+import alquilervehiculos.mvc.modelo.dominio.Cliente;
+import alquilervehiculos.mvc.modelo.dominio.ExcepcionAlquilerVehiculos;
+import alquilervehiculos.mvc.modelo.dominio.vehiculo.Vehiculo;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ *
+ * @author lol
+ */
+public class Alquileres
+{
+
+    private List<Alquiler> alquileres;
+    private final String PATH_FICHERO = "datos/alquileres.dat";
+
+    public Alquileres()
+    {
+        alquileres = new Vector<Alquiler>();
+    }
+
+    public void abrir(Cliente cliente, Vehiculo vehiculo)
+    {
+        if (vehiculo.getDisponible())
+        {
+            alquileres.add(new Alquiler(cliente, vehiculo));
+        } else
+        {
+            throw new ExcepcionAlquilerVehiculos("Vehículo no disponible.");
+        }
+    }
+
+    public void cerrar(String matricula)
+    {
+        if (alquilerEncontrado(matricula))
+        {
+            alquileres.get(posicionAlquiler(matricula)).close();
+        } else
+        {
+            throw new ExcepcionAlquilerVehiculos("Alquiler no encontrado.");
+        }
+    }
+
+    private boolean alquilerEncontrado(String matricula)
+    {
+       return alquileres.get(posicionAlquiler(matricula)).getVehiculo().getMatricula().equals(matricula);
+    }
+
+    public int posicionAlquiler(String matricula)
+    {
+        int i = 0;
+        while (i < alquileres.size() - 1)
+        {
+            if (alquileres.get(i).getVehiculo().getMatricula().equals(matricula)
+                    && alquileres.get(i).getAlquilerAbierto())
+            {
+                break;
+            } else
+            {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public Alquiler getAlquiler(String matricula)
+    {
+        if (alquilerEncontrado(matricula))
+        {
+            return alquileres.get(posicionAlquiler(matricula));
+        } else
+        {
+            throw new ExcepcionAlquilerVehiculos("Alquiler no encontrado.");
+        }
+    }
+
+    public List<Alquiler> getAlquileres()
+    {
+        return new Vector<Alquiler>(alquileres);
+    }
+
+    public List<Alquiler> getAbiertos()
+    {
+        int i = 0;
+        List<Alquiler> abiertos = new Vector<Alquiler>();
+        while (i < alquileres.size())
+        {
+            if (alquileres.get(i).getAlquilerAbierto())
+            {
+                abiertos.add(alquileres.get(i));
+            }
+            i++;
+        }
+        return abiertos;
+    }
+
+    public List<Alquiler> getAlquileresCliente(String dni)
+    {
+        int i = 0;
+        List<Alquiler> alquileresCliente = new Vector<Alquiler>();
+
+        while (i < alquileres.size())
+        {
+            if (alquileres.get(i).getCliente().getDni().equals(dni))
+            {
+                alquileresCliente.add(alquileres.get(i));
+            }
+            i++;
+        }
+        return alquileresCliente;
+    }
+
+    public List<Alquiler> getAlquileresVehiculo(String matricula)
+    {
+        int i = 0;
+        List<Alquiler> alquileresVehiculo = new Vector<Alquiler>();
+
+        while (i < alquileres.size())
+        {
+            if (alquileres.get(i).getVehiculo().getMatricula().equals(matricula))
+            {
+                alquileresVehiculo.add(alquileres.get(i));
+            }
+            i++;
+        }
+        return alquileresVehiculo;
+    }
+
+    public void escribirFicheroObjetos()
+    {
+
+    }
+
+    private File crearNevoFichero()
+    {
+        File fichero = new File(PATH_FICHERO);
+        return fichero;
+    }
+
+    public void leerObjetosDeFichero() //throws FileNotFoundException, EOFException, IOException, ClassNotFoundException. No lo tengo claro ¿Instead trycatch?
+    {
+
+    }
+
+}
